f1abf76a15799bc53ffcf48b2866ba66e852dc9c
ArtemShchuchev <shuaa@rambler.ru>
2024-10-11 15:38:49 +0700

    добавил не однократный обход матрицы

--- main.cpp
+++ main.cpp
@@ -50,6 +50,14 @@ static void crossFill(vector<string>& grid, const CrossPoint cp) {
     grid[cp.line][cp.row] = 'O';
 }
 
+static void restoreMatrix(vector<string>& grid) {
+    for (auto& line : grid) {
+        for (auto& ch : line) {
+            if (ch == 'O') ch = 'G';
+        }
+    }
+}
+
 static int twoPluses(vector<string> grid) {
     int sizeRow(static_cast<int>(grid[0].size()));
     int sizeLine(static_cast<int>(grid.size()));
@@ -57,67 +65,76 @@ static int twoPluses(vector<string> grid) {
     int minSize(min(sizeRow, sizeLine));
     int maxCrossing(minSize % 2 ? minSize : minSize - 1);
 
-    int numPoint(0);
-    CrossPoint cp[2]{ {-1,-1,-1}, {-1,-1,-1} };
-    int radius(maxCrossing / 2);
-    int tempRad(0);
+    vector<int> square;
+    int testRad(maxCrossing / 2);
+    while (testRad > 1) {
+        int radius(testRad);
+        --testRad;
 
-    while (radius >= 0) {
-        if (cp[numPoint].radius >= radius) {
-            if (numPoint) numPoint = 2;
-            else {
-                crossFill(grid, cp[numPoint]);
-                ++numPoint;
-            }
+        int numPoint(0);
+        CrossPoint cp[2]{ {-1,-1,-1}, {-1,-1,-1} };
+        int tempRad(0);
 
-            if (numPoint == 2) break;
-        }
-
-        int lMin = radius;
-        int rMin = radius;
-        int lMax = sizeLine - lMin - 1;
-        int rMax = sizeRow - rMin - 1;
-
-        vector<pair<int, int>> coords;
-
-        // horizontal
-        for (int r(rMin); r <= rMax; ++r) {
-            coords.push_back({ lMin, r });
-            if (lMin != lMax) coords.push_back({ lMax, r });
-        }
-        // vertikal
-        ++lMin;
-        --lMax;
-        for (int l(lMin); l <= lMax; ++l) {
-            coords.push_back({ l, rMin });
-            if (rMin != rMax) coords.push_back({ l, rMax });
-        }
-        // find cross
-        for (const auto& [l, r] : coords) {
-            tempRad = findRadiusCross(grid, l, r, radius);
-            if (tempRad >= 0) {
-                if (tempRad == radius) {
-                    cp[numPoint] = { l, r, tempRad };
+        while (radius >= 0) {
+            if (cp[numPoint].radius >= radius) {
+                if (numPoint) numPoint = 2;
+                else {
                     crossFill(grid, cp[numPoint]);
                     ++numPoint;
                 }
-                else if (tempRad > cp[numPoint].radius) cp[numPoint] = { l, r, tempRad };
+
                 if (numPoint == 2) break;
             }
+
+            int lMin = radius;
+            int rMin = radius;
+            int lMax = sizeLine - lMin - 1;
+            int rMax = sizeRow - rMin - 1;
+
+            vector<pair<int, int>> coords;
+
+            // horizontal
+            for (int r(rMin); r <= rMax; ++r) {
+                coords.push_back({ lMin, r });
+                if (lMin != lMax) coords.push_back({ lMax, r });
+            }
+            // vertikal
+            ++lMin;
+            --lMax;
+            for (int l(lMin); l <= lMax; ++l) {
+                coords.push_back({ l, rMin });
+                if (rMin != rMax) coords.push_back({ l, rMax });
+            }
+            // find cross
+            for (const auto& [l, r] : coords) {
+                tempRad = findRadiusCross(grid, l, r, radius);
+                if (tempRad >= 0) {
+                    if (tempRad == radius) {
+                        cp[numPoint] = { l, r, tempRad };
+                        crossFill(grid, cp[numPoint]);
+                        ++numPoint;
+                    }
+                    else if (tempRad > cp[numPoint].radius) cp[numPoint] = { l, r, tempRad };
+                    if (numPoint == 2) break;
+                }
+            }
+
+            if (numPoint == 2) break;
+
+            --radius;
         }
+        restoreMatrix(grid);
 
-        if (numPoint == 2) break;
-
-        --radius;
+        int square1 = cp[0].radius * 4 + 1;
+        int square2 = cp[1].radius * 4 + 1;
+        square.push_back(square1 * square2);
+        std::cout << square1 << " * " << square2 << " = " << square.back() << endl;
     }
 
 
-    int square1 = cp[0].radius * 4 + 1;
-    int square2 = cp[1].radius * 4 + 1;
 
-    std::cout << square1 << " * " << square2 << " = ";
 
-    return square1 * square2;
+    return *max_element(square.begin(), square.end());
 }
 
 
